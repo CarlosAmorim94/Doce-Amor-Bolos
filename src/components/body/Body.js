68f768ddc6de77ab304bab2813624f23e9931906
Carlos <carlos.av.amorim@gmail.com>
2021-11-04 19:34:13 -0300

    Website refeito

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import './Body.css'
+import cake from '../../img/cakehome.png'
 
 export default function Body() {
     return (
-        <div>
-            <h1>Body</h1>
+        <div id="body">
+            <div id="home">
+                <section id="texts">
+                    <div>
+                        <span>Feito com carinho!</span>
+                    </div>
+                    <div>
+                        <p>Deliciosos bolos feitos com carinho e dedicação, com o máximo de sabor e leveza que só a Doce Amor pode proporcionar</p>
+                    </div>
+                </section>
+                <section id="cake_image">
+                    <img src={cake} alt="Bolo delicioso"/>
+                </section>
+            </div>
+            
+            <div id="">Imagens</div>
         </div>
     )
 }
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import './Header.css'
 import Logo from '../../img/logo.jpg'
-import CakeHome from '../../img/cakehome.png'
+import Facebook from '../../img/facebook.png'
+import Whats from '../../img/whatsapp.png'
 
 export default function Header() {
     return (
@@ -10,26 +11,14 @@ export default function Header() {
                 <div id="logo">
                     <img  src={Logo} alt="logotipo da Doce Amor"/>
                 </div>
-                <nav>
+                <nav id="navbar">
                     <ul>
                         <li>Produtos</li>
                         <li>História</li>
-                        <li>Contato</li>
+                        <li><a href="https://www.facebook.com/Doce-amor-105933260819755"><img src={Facebook} alt="facebook"/></a></li>
+                        <li><a href="https://api.whatsapp.com/send?phone=5514991961016&app=facebook&entry_point=page_cta&fbclid=IwAR2vuMCgvS4IwHgX9l9EdvHpXXcfI-yE2g_OFl4rzlV93nyQC56MF3D3w7k"><img src={Whats} alt="whatsapp"/></a></li>
                     </ul>
                 </nav>
-            </div>
-            <div id="show">
-                <div id="texts">
-                    <h1 class="title">
-                        FEITO COM AMOR
-                    </h1>
-                    <p class="mintext">
-                        Deliciosos bolos feitos com carinho e dedicação, com o máximo de sabor e leveza que só a Doce Amor pode proporcionar
-                    </p>
-                </div>   
-                <div id="cake_home">
-                    <img id="img" src={CakeHome} alt="logotipo da Doce Amor"/>
-                </div>
             </div>
         </header>
     )
