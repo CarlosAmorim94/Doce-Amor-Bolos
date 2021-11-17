f3c948917303f7395b867056950e25ff6ba48b63
Carlos Amorim <carlos.av.amorim@gmail.com>
2021-11-17 19:40:37 -0300

    Modificando Cards

--- src/components/body/cards/allCards/Cards.js
+++ src/components/body/cards/allCards/Cards.js
@@ -15,22 +15,27 @@ export default function Cards(props) {
         setModalVisible(true)
     }
 
+    const closeModal = () => {
+        setModalVisible(false)
+    }
+
     return (
         <>
-            <div onClick={showModal} class="cards">
+            <div onClick={showModal}>
                 <img src={props.img} alt="imagem de um bolo"/>
             </div>
 
             <Modal visible={modalVisible} setVisible={setModalVisible}>
                 <img id="img_modal" src={props.img} alt="imagem de um bolo"/>
-                <nav id="navbar">
+                <nav>
                     <p>Experimente! entre em contato por nossas redes sociais:</p>
-                    <ul>
+                    <ul class="social">
                         <li><a href="https://www.facebook.com/Doce-amor-105933260819755"><img src={Facebook} alt="facebook"/></a></li>
-                        <li><a href="https://api.whatsapp.com/send?phone=5514991961016&app=facebook&entry_point=page_cta&fbclid=IwAR2vuMCgvS4IwHgX9l9EdvHpXXcfI-yE2g_OFl4rzlV93nyQC56MF3D3w7k"><img src={Whats} alt="whatsapp"/></a></li>
-                        <li><a href="https://www.instagram.com/doceamor98/"><img src={Instagram} alt="Instagram"/></a></li>
+                        <li class="social"><a href="https://api.whatsapp.com/send?phone=5514991961016&app=facebook&entry_point=page_cta&fbclid=IwAR2vuMCgvS4IwHgX9l9EdvHpXXcfI-yE2g_OFl4rzlV93nyQC56MF3D3w7k"><img src={Whats} alt="whatsapp"/></a></li>
+                        <li class="social"><a href="https://www.instagram.com/doceamor98/"><img src={Instagram} alt="Instagram"/></a></li>
                     </ul>
                 </nav>
+                <button onClick={closeModal} id="close_modal">Fechar</button>
             </Modal>
         </>
     )
