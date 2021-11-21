ff8924b0cb42e6cf046c1fcd4f6153959c96c669
Carlos Amorim <carlos.av.amorim@gmail.com>
2021-11-21 16:32:41 -0300

    Finalizado

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -2,6 +2,9 @@ import React from 'react'
 import './Body.css'
 import cake from '../../img/cakehome.png'
 import Allcards from './cards/Allcards'
+import Facebook from '../../img/facebook.png'
+import Whats from '../../img/whatsapp.png'
+import Instagram from '../../img/instagram.png'
 
 export default function Body() {
 
@@ -12,9 +15,17 @@ export default function Body() {
                     <div>
                         <span>Feito com amor!</span>
                     </div>
-                    <div>
-                        <p>Deliciosos bolos feitos com carinho e dedicação, com o máximo de sabor e leveza que só a Doce Amor pode proporcionar! Entre em contato e experimente!</p>
-                        <p>Você pode personalizar seu bolo como desejar! Sabores formatos, enfeites e cores! Consulte todas as variedades em nossas redes sociais!</p>
+                    <div id="redes">
+                        <p>DELICIOSOS BOLOS FEITOS COM CARINHO E O MáXIMO DE LEVEZA e SABOR QUE Só A DOCE AMOR PODE PROPORCIONAR! <br/><br/>VOCê PODE PERSONALIZAR SEU BOLO COMO DESEJAR! SABORES FORMATOS, ENFEITES E CORES ENTRE EM CONTATOR POR NOSSAS REDES SOCIAIS:</p>
+                        <div>
+                            <nav id="navbar">
+                                <ul>
+                                    <li><a href="https://www.facebook.com/Doce-amor-105933260819755"><img src={Facebook} alt="facebook"/></a></li>
+                                    <li><a href="https://api.whatsapp.com/send?phone=5514991961016&app=facebook&entry_point=page_cta&fbclid=IwAR2vuMCgvS4IwHgX9l9EdvHpXXcfI-yE2g_OFl4rzlV93nyQC56MF3D3w7k"><img src={Whats} alt="whatsapp"/></a></li>
+                                    <li><a href="https://www.instagram.com/doceamor98/"><img src={Instagram} alt="Instagram"/></a></li>
+                                </ul>
+                            </nav>
+                        </div>
                     </div>
                 </section>
                 <section id="cake_image">
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,10 +1,6 @@
 import React from 'react'
 import './Footer.css'
 import Logo from '../../img/logo.jpg'
-import Facebook from '../../img/facebook.png'
-import Whats from '../../img/whatsapp.png'
-import Instagram from '../../img/instagram.png'
-
 
 export default function Footer() {
     return (
@@ -13,19 +9,10 @@ export default function Footer() {
                 <div id="logo">
                     <img  src={Logo} alt="logotipo da Doce Amor"/>
                 </div>
-                <div>
-                    <h2>Doce Amor</h2>
-                    <p>Localizada em São Manuel - SP</p>
+                <div id="text">
+                    <p>Doce Amor&copy; - Todos os direitos reservados</p>
                     <p>Site desenvolvido por: <a href="https://www.linkedin.com/in/carlos-amorim-9a9a8aa2/">Carlos Amorim</a></p>
                 </div>
-                <nav id="navbar">
-                    <h1>Entre em contato via:</h1>
-                    <ul>
-                        <li><a href="https://www.facebook.com/Doce-amor-105933260819755"><img src={Facebook} alt="facebook"/></a></li>
-                        <li><a href="https://api.whatsapp.com/send?phone=5514991961016&app=facebook&entry_point=page_cta&fbclid=IwAR2vuMCgvS4IwHgX9l9EdvHpXXcfI-yE2g_OFl4rzlV93nyQC56MF3D3w7k"><img src={Whats} alt="whatsapp"/></a></li>
-                        <li><a href="https://www.instagram.com/doceamor98/"><img src={Instagram} alt="Instagram"/></a></li>
-                    </ul>
-                </nav>
             </div>           
         </div>
     )
